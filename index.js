836d8e8efe8b5903b5e9aeeeac6aa9c0dabca759
Shivani Jadhav <157908746+Shivpjadhav@users.noreply.github.com>
2026-02-15 20:22:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,4 +25,11 @@ const githubdata={
   "email": null, 
   "hireable": null,
   "bio": "I make coding videos on youtube and for courses. My youtube channel explains my work more. Check that out", 
-  "twitter_username": "hiteshdotcom", "public_repos": 110, "public_gists": 5, "followers": 45482, "following": 0, "created_at": "2015-03-23T13:03:25Z", "updated_at": "2025-07-01T03:18:33Z" } app.get('/',(req,res)=>{ res.send('hello world') }) app.get('/tiwitter',(req,res)=>{ res.send('hello tiwitter') }) app.get('/login',(req,res)=>{ res.send('hello login') }) app.get('/githubdata',(req,res)=>{ res.json(githubdata) }) app.listen(process.env.PORT,()=>{ console.log(`listenning on port ${port}`); })
+  "twitter_username": "hiteshdotcom",
+  "public_repos": 110,
+  "public_gists": 5,
+  "followers": 45482, 
+  "following": 0, 
+  "created_at": "2015-03-23T13:03:25Z", 
+  "updated_at": "2025-07-01T03:18:33Z" }
+  app.get('/',(req,res)=>{ res.send('hello world') }) app.get('/tiwitter',(req,res)=>{ res.send('hello tiwitter') }) app.get('/login',(req,res)=>{ res.send('hello login') }) app.get('/githubdata',(req,res)=>{ res.json(githubdata) }) app.listen(process.env.PORT,()=>{ console.log(`listenning on port ${port}`); })
